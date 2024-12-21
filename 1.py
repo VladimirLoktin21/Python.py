e1ecefda39f47397ee94b97ca03a85c48e68e156
VladimirLoktin21 <142094719+VladimirLoktin21@users.noreply.github.com>
2024-12-21 10:17:58 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,14 @@
-asda
+class IterableWithGenerator:
+    def __init__(self, data):
+        self.data = data
+
+    def __iter__(self):
+        # Возвращает генератор при каждой итерации
+        return (item for item in self.data)
+
+# Пример использования
+my_iterable = IterableWithGenerator([1, 2, 3, 4, 5])
+
+# Итерация по объекту
+for value in my_iterable:
+    print(value)
